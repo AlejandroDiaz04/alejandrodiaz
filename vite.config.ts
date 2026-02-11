1cf7444026a5800bcab360aa824c113e0d8f9ce1
Alejandro Díaz <alejandrodiaz04zr@gmail.com>
2026-02-10 22:38:52 -0300

    update portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/alejandrodiaz/",
   plugins: [react()],
-  base: "https://alejandrodiaz04.github.io/alejandrodiaz/",
 });
